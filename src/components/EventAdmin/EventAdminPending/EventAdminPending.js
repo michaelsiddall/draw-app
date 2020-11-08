bd2be589d6eacee2fdb4ccad01ab3680c4e4b76c
Emma Westbrook <emmalwestbrook@gmail.com>
2020-11-07 18:55:35 -0600

    added styling to show images with approve/disapprove buttons. on click they show name

--- src/components/EventAdmin/EventAdminPending/EventAdminPending.js
+++ src/components/EventAdmin/EventAdminPending/EventAdminPending.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../../redux/mapStoreToProps';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-
+import EventAdminPendingCard from './EventAdminPendingCard';
+import './EventAdminPending.css';
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
@@ -17,16 +12,16 @@ class EventAdminPending extends Component {
         heading: 'Pending',
     };
 
-    componentDidMount() {
-        const action = { type: 'GET_PENDING_DRAWINGS' };
-        this.props.dispatch(action);
-    };
-
     render() {
         return (
             <div>
                 <h2>{this.state.heading}</h2>
-                {JSON.stringify(this.props.store.drawing)}
+                {/* {JSON.stringify(this.props.store.drawing)} */}
+                <div className="pendingGrid">
+                    {this.props.store.drawing.map((drawing) => {
+                        return (<EventAdminPendingCard drawing={drawing} />);
+                    })}
+                </div>
             </div>
         );
     }
--- src/components/EventAdmin/EventAdminPending/EventAdminPendingCard.js
+++ src/components/EventAdmin/EventAdminPending/EventAdminPendingCard.js
@@ -14,14 +14,34 @@ import Button from '@material-ui/core/Button';
 // component.
 class EventAdminPendingCard extends Component {
     state = {
-        heading: 'Pending',
-    };
+        isClicked: true
+    }
+    //when image is clicked, state is updated to its current opposite
+    clickText = () => {
+        console.log("showing text");
+        this.setState({
+            isClicked: !this.state.isClicked
+        });
+    }
 
     render() {
         return (
-            <Card>
-                <h2>{this.state.heading}</h2>
-            </Card>
+            <div>
+                <Card className="pendingItem" key={this.props.drawing.id}>
+                    <div onClick={this.clickText}>
+                        {this.state.isClicked ?
+                            <img src={this.props.drawing.image_url} alt="image" height="200" /> :
+                            <div className="imageText"><h2>{this.props.drawing.name}</h2></div>
+                        }
+                    </div>
+
+                    <CardActions>
+                        <Button size="medium" color="primary">approve</Button>
+                        <Button size="medium" color="primary">disapprove</Button>
+                    </CardActions>
+                </Card>
+            </div>
+
         );
     }
 }
