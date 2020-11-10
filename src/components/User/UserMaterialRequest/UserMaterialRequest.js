4d9d6d48014032dba82e0be3e2333bff93ea4d5a
Mike Siddall <eatingartist@gmail.com>
2020-11-10 11:25:45 -0600

    updated MaterialRequest Companent with original code

--- src/components/User/UserMaterialRequest/UserMaterialRequest.js
+++ src/components/User/UserMaterialRequest/UserMaterialRequest.js
@@ -1,86 +1,94 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import mapStoreToProps from "../../../redux/mapStoreToProps";
-import "./UserMaterialRequest.css";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-// Basic class component structure for React with default state
-// value setup. When making a new component be sure to replace
-// the component name TemplateClass with the name for the new
-// component.
+import mapStoreToProps from '../../../redux/mapStoreToProps';
+import './UserMaterialRequest.css';
+
 class UserMaterialRequest extends Component {
-    state = {
-        materialRequest: {
-            location: null,
-            tableNumber: null,
-            artistNumber: null,
-        },
-    };
+  state = {
+    materialRequest: {
+      location: null,
+      tableNumber: null,
+      artistNumber: null,
+    },
+  };
 
-    onChange = (event, property) => {
-        console.log("payload is", property, event.target.value);
-        this.setState({
-            materialRequest: {
-                ...this.state.materialRequest,
-                [property]: event.target.value,
-            },
-        });
-    };
+  componentDidMount = () => {
+    console.log('in componentDidMount');
+    this.props.dispatch({
+      type: 'FETCH_EVENTS',
+    });
+  };
 
-    onSubmit = () => {
-        console.log("This is the materials request", this.state.materialRequest);
+  onChange = (event, property) => {
+    console.log('payload is', property, event.target.value);
+    this.setState({
+      materialRequest: {
+        ...this.state.materialRequest,
+        [property]: event.target.value,
+      },
+    });
+  };
 
-        this.props.dispatch({
-            type: "ADD_REQUEST",
-            payload: this.state.materialRequest,
-        });
-    };
+  onSubmit = () => {
+    console.log('This is the materials request', this.state.materialRequest);
 
-    render() {
-        console.log("redux state is", this.props.store);
-        return (
-            <div>
-                <h2 className="centered">Material Request</h2>
+    this.props.dispatch({
+      type: 'ADD_REQUEST',
+      payload: this.state.materialRequest,
+    });
+  };
 
-                <h5 className="centered">Location</h5>
-                <select
-                    className="inputCentered"
-                    defaultValue={"DEFAULT"}
-                    onChange={(event) => this.onChange(event, "location")}
-                >
-                    <option value="DEFAULT" disabled>
-                        Select Event
+  render() {
+    console.log('redux state is', this.props.store);
+    return (
+      <div>
+        <h2 className='centered'>Material Request</h2>
+
+        <h5 className='centered'>Location</h5>
+        <select
+          className='selectCentered'
+          defaultValue={'DEFAULT'}
+          onChange={(event) => this.onChange(event, 'location')}
+        >
+          <option value='DEFAULT' disabled>
+            Select Event
           </option>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                </select>
+          {this.props.store.eventsReducer.map((event) => {
+            return (
+              <option key={event.id} value={event.id}>
+                {event.location}
+              </option>
+            );
+          })}
+        </select>
 
-                <h5 className="centered">Table Number</h5>
-                <input
-                    className="inputCentered"
-                    type="number"
-                    placeholder="Select a Table"
-                    min="1"
-                    max="99"
-                    onChange={(event) => this.onChange(event, "tableNumber")}
-                ></input>
+        <h5 className='centered'>Table Number</h5>
+        <input
+          className='inputCentered'
+          type='number'
+          placeholder='Select a Table'
+          min='1'
+          max='99'
+          onChange={(event) => this.onChange(event, 'tableNumber')}
+        ></input>
 
-                <h5 className="centered">Number of Artists</h5>
-                <input
-                    className="inputCentered"
-                    type="number"
-                    placeholder="Select a Number"
-                    min="1"
-                    max="15"
-                    onChange={(event) => this.onChange(event, "artistNumber")}
-                ></input>
-                <br></br>
-                <button className="buttonCentered" onClick={this.onSubmit}>
-                    Request Drawing Materials
+        <h5 className='centered'>Number of Artists</h5>
+        <input
+          className='inputCentered'
+          type='number'
+          placeholder='Select a Number'
+          min='1'
+          max='15'
+          onChange={(event) => this.onChange(event, 'artistNumber')}
+        ></input>
+        <br></br>
+        <button className='buttonCentered' onClick={this.onSubmit}>
+          Request Drawing Materials
         </button>
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default connect(mapStoreToProps)(UserMaterialRequest);
