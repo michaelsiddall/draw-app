5d3aa5936d0cba60b8e81b17ccc65f5ceea3f398
Jake Stucky <jakestucky@gmnail.com>
2020-11-12 10:00:51 -0600

    added image upload and got it pushing to db

--- src/components/User/UserDrawingSubmit/UserDrawingSubmit.js
+++ src/components/User/UserDrawingSubmit/UserDrawingSubmit.js
@@ -8,93 +8,111 @@ import Input from '@material-ui/core/Input';
 // component.
 import '../UserMaterialRequest/UserMaterialRequest.css';
 import Swal from 'sweetalert2';
+import ImageUpload from '../../ImageUpload/ImageUpload';
 
 class UserDrawingSubmit extends Component {
-    state = {
-        drawingSubmit: {
-            name: null,
-            email: null,
-            instagram: null,
-            aboutDrawing: null,
-            imageUrl: 'https://upload.wikimedia.org/wikipedia/en/4/4d/Shrek_%28character%29.png'
-        },
-    };
+  state = {
+    drawingSubmit: {
+      name: null,
+      email: null,
+      instagram: null,
+      aboutDrawing: null,
+      imageUrl: this.props.store.imageUrlReducer,
+    },
+  };
 
-    onChange = (event, property) => {
-        console.log('payload is', property, event.target.value);
-        this.setState({
-            drawingSubmit: {
-                ...this.state.drawingSubmit,
-                [property]: event.target.value,
-            },
+  onChange = (event, property) => {
+    console.log('payload is', property, event.target.value);
+
+    this.setState({
+      drawingSubmit: {
+        ...this.state.drawingSubmit,
+        [property]: event.target.value,
+      },
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log('This is the materials request', this.state.drawingSubmit);
+    Swal.fire({
+      title: 'Are you sure your drawing is ready to submit?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#9dac68',
+      cancelButtonColor: '#e26d5c',
+      confirmButtonText: 'Yes, submit my drawing!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.props.dispatch({
+          type: 'POST_DRAWING',
+          payload: {
+            name: this.state.drawingSubmit.name,
+            email: this.state.drawingSubmit.email,
+            instagram: this.state.drawingSubmit.instagram,
+            aboutDrawing: this.state.drawingSubmit.aboutDrawing,
+            imageUrl: this.props.store.imageUrlReducer,
+          },
         });
-    };
-
-    onSubmit = (event) => {
-        event.preventDefault();
-        console.log('This is the materials request', this.state.drawingSubmit);
-
         Swal.fire({
-            title: 'Are you sure your drawing is ready to submit?',
-            icon: 'question',
-            showCancelButton: true,
-            confirmButtonColor: '#9dac68',
-            cancelButtonColor: '#e26d5c',
-            confirmButtonText: 'Yes, submit my drawing!',
-        }).then((result) => {
-            if (result.isConfirmed) {
-                this.props.dispatch({
-                    type: 'POST_DRAWING',
-                    payload: this.state.drawingSubmit,
-                });
-                Swal.fire({
-                    title: "Thank you for helping Draw's cause!",
-                    showConfirmButton: false,
-                    timer: 3000,
-                });
-            }
+          title: "Thank you for helping Draw's cause!",
+          showConfirmButton: false,
+          timer: 3000,
         });
-    }
+      }
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.onSubmit}>
-                    <h5 className='centered'>Name</h5>
-                    <input
-                        className='inputCentered'
-                        type='text'
-                        placeholder='name'
-                        onChange={(event) => this.onChange(event, 'name')}
-                    ></input>
-                    <h5 className='centered'>Email (if you'd like to learn about future Draw events)</h5>
-                    <input
-                        className='inputCentered'
-                        type='text'
-                        placeholder='email'
-                        onChange={(event) => this.onChange(event, 'email')}
-                    ></input>
-                    <h5 className='centered'>Instagram Handle (if you'd like credit for your work if we post it!)</h5>
-                    <input
-                        className='inputCentered'
-                        type='text'
-                        placeholder='instagram handle'
-                        onChange={(event) => this.onChange(event, 'instagram')}
-                    ></input>
-                    <h5 className='centered'>About your drawing... add some info if you'd like!</h5>
-                    <input
-                        className='inputCentered'
-                        type='text'
-                        placeholder='about'
-                        onChange={(event) => this.onChange(event, 'aboutDrawing')}
-                    ></input>
-                    <br></br>
-                    <button type="submit" className='buttonCentered' >
-                        Submit Drawing!</button>
-                </form>
-            </div>
-        );
-    };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <h5 className='centered'>Name</h5>
+          <input
+            className='inputCentered'
+            type='text'
+            placeholder='name'
+            onChange={(event) => this.onChange(event, 'name')}
+          ></input>
+          <h5 className='centered'>
+            Email (if you'd like to learn about future Draw events)
+          </h5>
+          <input
+            className='inputCentered'
+            type='text'
+            placeholder='email'
+            onChange={(event) => this.onChange(event, 'email')}
+          ></input>
+          <h5 className='centered'>
+            Instagram Handle (if you'd like credit for your work if we post it!)
+          </h5>
+          <input
+            className='inputCentered'
+            type='text'
+            placeholder='instagram handle'
+            onChange={(event) => this.onChange(event, 'instagram')}
+          ></input>
+          <h5 className='centered'>
+            About your drawing... add some info if you'd like!
+          </h5>
+          <input
+            className='inputCentered'
+            type='text'
+            placeholder='about'
+            onChange={(event) => this.onChange(event, 'aboutDrawing')}
+          ></input>
+          <br></br>
+          <div className='inputCentered'>
+            <ImageUpload />
+          </div>
+
+          <button type='submit' className='buttonCentered'>
+            Submit Drawing!
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default connect(mapStoreToProps)(UserDrawingSubmit);
