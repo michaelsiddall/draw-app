054c0fc3d7157294037e7e2eb23915bb8bd6094a
Mike Siddall <eatingartist@gmail.com>
2020-11-16 20:43:02 -0600

    EventAdminQueue reloads every minute to update Event Admin of any newly submitted material requests

--- src/components/EventAdmin/EventAdminQueue/QueueItem/QueueItem.js
+++ src/components/EventAdmin/EventAdminQueue/QueueItem/QueueItem.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../../../redux/mapStoreToProps';
-import QueueComplete from "../QueueConfirm/QueueComplete";
-import QueueDelete from "../QueueConfirm/QueueDelete";
+import QueueComplete from '../QueueConfirm/QueueComplete';
+import QueueDelete from '../QueueConfirm/QueueDelete';
 
 class QueueItem extends Component {
+  componentDidMount = () => {
+    console.log('in componentDidMount');
+
+    // page reloads every minute to allow EventAdmin to see new Material Requests
+    window.setInterval(this.refresh, 60000);
+  };
+  // Refresh or reload page.
+  refresh = () => {
+    window.location.reload();
+  };
 
   render() {
     return (
@@ -12,22 +22,25 @@ class QueueItem extends Component {
         <tr>
           <td>{this.props.item.table_number}</td>
           <td>{this.props.item.artist_count}</td>
-          <td><QueueComplete
-              tableNumber= {this.props.item.table_number}
-              artistCount = {this.props.item.artist_count}
+          <td>
+            <QueueComplete
+              tableNumber={this.props.item.table_number}
+              artistCount={this.props.item.artist_count}
               item={this.props.item}
-              eventID = {this.props.eventID}
-          /></td>
-          <td><QueueDelete
-              tableNumber= {this.props.item.table_number}
-              artistCount = {this.props.item.artist_count}
+              eventID={this.props.eventID}
+            />
+          </td>
+          <td>
+            <QueueDelete
+              tableNumber={this.props.item.table_number}
+              artistCount={this.props.item.artist_count}
               item={this.props.item}
-              eventID = {this.props.eventID}
-          /></td>
+              eventID={this.props.eventID}
+            />
+          </td>
         </tr>
       </>
     );
-
   }
 }
 
