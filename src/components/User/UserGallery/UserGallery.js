852f4567b6f88033bb645964cb3b672676fb1ba1
Emma Westbrook <emmalwestbrook@gmail.com>
2020-11-17 18:10:46 -0600

    ok there's a grid list

--- src/components/User/UserGallery/UserGallery.js
+++ src/components/User/UserGallery/UserGallery.js
@@ -6,6 +6,12 @@ import './UserGallery.css';
 import HomeIcon from '@material-ui/icons/Home';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
@@ -32,9 +38,29 @@ class UserGallery extends Component {
                 />
                 <h2 className='title'>Gallery</h2>
                 <div className="pendingGrid">
-                    {this.props.store.approved.map((drawing) => {
-                        return (<UserGalleryCard drawing={drawing} />);
-                    })}
+                    <GridList cellHeight={180}>
+
+
+                        {this.props.store.approved.map((drawing) => {
+                            return (
+                                <GridListTile key={drawing.id}>
+                                    <img src={drawing.image_url} alt="drawing" />
+                                    <GridListTileBar
+                                        title={drawing.name}
+                                        subtitle={<span> {drawing.about}</span>}
+                                        actionIcon={
+                                            <IconButton aria-label={`info`} >
+                                                <InfoIcon />
+                                            </IconButton>
+                                        }
+                                    />
+                                </GridListTile>
+                            );
+
+
+                            // return (<UserGalleryCard drawing={drawing} />);
+                        })}
+                    </GridList>
                 </div>
             </div >
         );
