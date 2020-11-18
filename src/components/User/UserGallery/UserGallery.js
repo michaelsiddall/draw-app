6b5ac13addc76538112100f0943c0d7263801879
Emma Westbrook <emmalwestbrook@gmail.com>
2020-11-17 18:04:17 -0600

    last commit before getting down and dirty with the grid list

--- src/components/User/UserGallery/UserGallery.js
+++ src/components/User/UserGallery/UserGallery.js
@@ -3,26 +3,34 @@ import { connect } from 'react-redux';
 import mapStoreToProps from '../../../redux/mapStoreToProps';
 import UserGalleryCard from './UserGalleryCard';
 import './UserGallery.css';
+import HomeIcon from '@material-ui/icons/Home';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
 // component.
 class UserGallery extends Component {
-    state = {
-        heading: 'Gallery',
-    };
+
 
     componentWillMount() {
         this.props.dispatch({ type: 'GET_APPROVED_DRAWINGS' });
 
 
     }
+    goHome = () => {
+        this.props.history.push('/userhome');
+    };
 
     render() {
         return (
-            <div>
-                <h2>{this.state.heading}</h2>
+            <div className="centered">
+                <HomeIcon
+                    fontSize="large"
+                    style={{ color: '#577590' }}
+                    onClick={this.goHome}
+                />
+                <h2 className='title'>Gallery</h2>
                 <div className="pendingGrid">
                     {this.props.store.approved.map((drawing) => {
                         return (<UserGalleryCard drawing={drawing} />);
