c89d475e28e5954dced5bcb03a4489636b552276
Sophie <primesophie.2020@gmail.com>
2020-11-21 10:07:49 -0600

    welcome switch

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -4,6 +4,13 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 import Nav from '../Nav/Nav';
 import "./Welcome.css"
 import MailIcon from '@material-ui/icons/Mail';
+import {
+  HashRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
 
 class Welcome extends Component {
 
@@ -31,34 +38,17 @@ class Welcome extends Component {
 
             else if (this.props.store.user.auth_level==="admin"){
               return (
-                <div className="welcome-div">
-                  <div className="transluscent-welcome">
-                    <Nav/>
-                          <div id="user-page-container">
-                                    <h1 className="user-page-h1">Welcome, {this.props.store.user.username}!</h1>
-                                    <p className="user-page-p">Your ID is: {this.props.store.user.id}</p>
-                                    <p>Authorization Level: Admin</p>
-                                    <p> Please speak to the site administrator about any issues you may have or 
-                                      if you've forgotten your login and/or password!</p>
-                                    <p>{<MailIcon id="welcome-mail-icon" onClick={this.mail}/>}</p>
-                          </div>
-                    </div>
-                </div>
+                <Switch>
+                  <Redirect exact from='/welcome' to='/events' />
+                </Switch>
               )
             }
 
             else if (this.props.store.user.auth_level==="superAdmin"){
               return (
-                <div className="welcome-div">
-                  <div className="transluscent-welcome">
-                      <Nav/>
-                            <div id="user-page-container">
-                                      <h1 className="user-page-h1">Welcome, {this.props.store.user.username}!</h1>
-                                      <p className="user-page-p">Your ID is: {this.props.store.user.id}</p>
-                                      <p>Authorization Level: Super Admin</p>
-                            </div>
-                    </div>
-                </div>
+                <Switch>
+                  <Redirect exact from='/welcome' to='/events' />
+                </Switch>
               )
             }
 
