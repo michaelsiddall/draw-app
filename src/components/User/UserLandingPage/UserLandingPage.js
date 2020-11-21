d27a5c5d687155e6c23494183b11a41190822c3c
Mike Siddall <eatingartist@gmail.com>
2020-11-21 10:23:10 -0600

    added comments to UserLandingPage and UserMaterialRequest

--- src/components/User/UserLandingPage/UserLandingPage.js
+++ src/components/User/UserLandingPage/UserLandingPage.js
@@ -8,70 +8,88 @@ import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/styles';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 
-import Footer from "../../Footer/Footer"
-
-
+import Footer from '../../Footer/Footer';
 
 class UserLandingPage extends Component {
   state = {};
 
+  // redirects User to Gallery Page displaying all approved images
   viewDrawing = () => {
     this.props.history.push('/gallery');
   };
 
+  // redirects User to Submit Page allowing upload and submission of image for approval
   submitDrawing = () => {
     this.props.history.push('/submit');
   };
+
+  // redirects User to Material Request Page allowing User to submit drawing materials
   requestMaterial = () => {
     this.props.history.push('/request');
   };
 
   render() {
     return (
-      <div id="app-container">
-      <div className="parentDiv">
-        {/* <div className='drawLogo' maxWidth='med'>
+      <div id='app-container'>
+        <div className='parentDiv'>
+          {/* <div className='drawLogo' maxWidth='med'>
           <img src={drawlogo} className='logo' alt='draw logo' />
         </div> */}
-        <div >
-          <h2 className='centered title'>About Draw</h2>
-          <p className='copytext'>
-            Draw is a community color therapy initiative inspired by Charles Moertel, who wanted to make a children’s book,
-            but passed away before having the opportunity to achieve his goal.
-          </p>
-          <p className='copytext'>
-            Draw partners with local organizations to host coloring days where people express creativity, share stories,
-            and grow community by adding pages to the ever-growing children’s book.
-        </p>
+          <div>
+            <h2 className='centered title'>About Draw</h2>
+            <p className='copytext'>
+              Draw is a community color therapy initiative inspired by Charles
+              Moertel, who wanted to make a children’s book, but passed away
+              before having the opportunity to achieve his goal.
+            </p>
+            <p className='copytext'>
+              Draw partners with local organizations to host coloring days where
+              people express creativity, share stories, and grow community by
+              adding pages to the ever-growing children’s book.
+            </p>
+          </div>
+
+          <div className='centered'>
+            <div className='buttonDiv'>
+              <Button
+                id='landingButton'
+                size='large'
+                onClick={this.requestMaterial}
+              >
+                Request Materials
+              </Button>
+            </div>
+
+            <div className='buttonDiv'>
+              <Button
+                id='landingButton'
+                size='large'
+                onClick={this.submitDrawing}
+              >
+                Submit Drawing
+              </Button>
+            </div>
+
+            <div className='buttonDiv'>
+              <Button
+                id='landingButton'
+                size='large'
+                onClick={this.viewDrawing}
+              >
+                View Drawings
+              </Button>
+            </div>
+
+            <div className='buttonDiv'>
+              <a href='https://www.givemn.org/story/Draw' target='_blank'>
+                <Button id='landingButton' size='large'>
+                  Donate!
+                </Button>
+              </a>
+            </div>
+          </div>
         </div>
-
-        <div className="centered">
-          <div className="buttonDiv">
-            <Button id="landingButton" size="large" onClick={this.requestMaterial}>
-              Request Materials
-           </Button>
-          </div>
-
-          <div className="buttonDiv">
-            <Button id="landingButton" size="large" onClick={this.submitDrawing}>
-              Submit Drawing
-          </Button>
-          </div>
-
-          <div className="buttonDiv">
-            <Button id="landingButton" size="large" onClick={this.viewDrawing}>
-              View Drawings
-          </Button>
-          </div>
-
-          <div className="buttonDiv">
-            <a href='https://www.givemn.org/story/Draw' target='_blank'>
-              <Button id="landingButton" size="large" >Donate!</Button>
-            </a>
-          </div>
-        </div>
-      </div >
-            <Footer/>
+        <Footer />
       </div>
     );
   }
--- src/components/User/UserMaterialRequest/UserMaterialRequest.js
+++ src/components/User/UserMaterialRequest/UserMaterialRequest.js
@@ -12,169 +12,164 @@ import './UserMaterialRequest.css';
 import Swal from 'sweetalert2';
 import '../UserStyles.css';
 import { InputLabel, MenuItem } from '@material-ui/core';
-import Footer from "../../Footer/Footer"
-
+import Footer from '../../Footer/Footer';
 
 const theme = createMuiTheme({
-    overrides: {
-        MuiButton: {
-            // Name of the rule
-            text: {
-                // Some CSS
-                background: '#f9844a',
-                borderRadius: 3,
-                border: 1,
-                borderColor: '#90BE6D',
-                color: 'white',
-                fontFamily: 'Work Sans',
-                textTransform: 'none',
-                height: 48,
-                padding: '0 30px',
-                "&:hover": {
-                    backgroundColor: "#F3722C"
-                },
-                //boxShadow: '0 3px 3px 2px #577590',
-            },
+  overrides: {
+    MuiButton: {
+      // Name of the rule
+      text: {
+        // Some CSS
+        background: '#f9844a',
+        borderRadius: 3,
+        border: 1,
+        borderColor: '#90BE6D',
+        color: 'white',
+        fontFamily: 'Work Sans',
+        textTransform: 'none',
+        height: 48,
+        padding: '0 30px',
+        '&:hover': {
+          backgroundColor: '#F3722C',
         },
+        //boxShadow: '0 3px 3px 2px #577590',
+      },
     },
+  },
 });
 
 class UserMaterialRequest extends Component {
-    state = {
-        materialRequest: {
-            location: null,
-            tableNumber: null,
-            artistNumber: null,
-        },
-    };
+  state = {
+    materialRequest: {
+      location: null,
+      tableNumber: null,
+      artistNumber: null,
+    },
+  };
 
+  // refreshes all events from db allowing to map through them & select in drop down inside form
+  componentDidMount = () => {
+    console.log('in componentDidMount');
+    this.props.dispatch({
+      type: 'FETCH_EVENTS',
+    });
+  };
 
+  // captures location as EventId
+  onChange = (event, property) => {
+    console.log('payload is', property, event.target.value);
+    this.setState({
+      materialRequest: {
+        ...this.state.materialRequest,
+        [property]: event.target.value,
+      },
+    });
+  };
+  // redirect user pack to UserLandingPage after submit
+  goHome = () => {
+    this.props.history.push('/userhome');
+  };
 
-    componentDidMount = () => {
-        console.log('in componentDidMount');
-        this.props.dispatch({
-            type: 'FETCH_EVENTS',
-        });
-    };
+  // sends Material Request, requires all fields to be entered or get alert, will ask to confirm submission
 
-
-    onChange = (event, property) => {
-        console.log('payload is', property, event.target.value);
-        this.setState({
-            materialRequest: {
-                ...this.state.materialRequest,
-                [property]: event.target.value,
-            },
-        });
-    };
-
-    goHome = () => {
-        this.props.history.push('/userhome');
-    };
-
-    onSubmit = (event) => {
-        event.preventDefault();
-        console.log('This is the materials request', this.state.materialRequest);
-        if (
-            this.state.materialRequest.location !== null &&
-            this.state.materialRequest.tableNumber !== null &&
-            this.state.materialRequest.artistNumber !== null
-        ) {
-            Swal.fire({
-                title: 'Are you ready to submit your materials request?',
-                // text: this.state.materialRequest.location,
-                icon: 'question',
-                showCancelButton: true,
-                confirmButtonColor: '#9dac68',
-                cancelButtonColor: '#e26d5c',
-                confirmButtonText: 'Yes, send request!',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    this.props.dispatch({
-                        type: 'ADD_REQUEST',
-                        payload: this.state.materialRequest,
-                    });
-                    Swal.fire({
-                        title: 'Drawing materials will be delivered to your table shortly!',
-                        showConfirmButton: false,
-                        timer: 3000,
-                    });
-                    this.props.history.push('/userhome');
-
-                }
-            });
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log('This is the materials request', this.state.materialRequest);
+    if (
+      this.state.materialRequest.location !== null &&
+      this.state.materialRequest.tableNumber !== null &&
+      this.state.materialRequest.artistNumber !== null
+    ) {
+      Swal.fire({
+        title: 'Are you ready to submit your materials request?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#9dac68',
+        cancelButtonColor: '#e26d5c',
+        confirmButtonText: 'Yes, send request!',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.props.dispatch({
+            type: 'ADD_REQUEST',
+            payload: this.state.materialRequest,
+          });
+          Swal.fire({
+            title: 'Drawing materials will be delivered to your table shortly!',
+            showConfirmButton: false,
+            timer: 3000,
+          });
+          this.props.history.push('/userhome');
         }
-    };
-    render() {
-        console.log('redux state is', this.props.store);
-        return (
-            <div id="app-container">
-            <form className="centered">
-                <MuiThemeProvider theme={theme}>
-                    <HomeIcon
-                        fontSize="large"
-                        style={{ color: '#577590' }}
-                        onClick={this.goHome}
-                    />
-                    <h2 className='title'>Get Drawing Materials!</h2>
-                    <div className='centered'>
-                        <h5 className='smallerTitle'>Location</h5>
-                        <InputLabel id="location">Where is your event?</InputLabel>
-                        <Select
-                            required
-                            id="location"
-                            defaultValue={''}
-                            style={{ minWidth: 200 }}
-                            onChange={(event) => this.onChange(event, 'location')}
-                        >
-                            <MenuItem value='' disabled>
-                                Select Event </MenuItem>
-                            {this.props.store.eventReducer.map((event) => {
-                                return (
-                                    <MenuItem key={event.id} value={event.id}>
-                                        {event.location}
-                                    </MenuItem>
-                                );
-                            })}
-                        </Select>
-                    </div>
-
-                    <div className='centered'>
-                        <h5 className='centered smallerTitle'>Table Number</h5>
-                        <Input
-                            style={{ minWidth: 200 }}
-                            type='number'
-                            placeholder='Table Number'
-                            onChange={(event) => this.onChange(event, 'tableNumber')}
-                            required
-                        ></Input>
-                    </div>
-
-                    <div className='centered'>
-                        <h5 className='smallerTitle'>Number of Participants</h5>
-                        <Input
-                            style={{ minWidth: 200 }}
-
-                            type='number'
-                            placeholder='Select a Number'
-                            onChange={(event) => this.onChange(event, 'artistNumber')}
-                            required
-                        ></Input>
-                    </div>
-                    <div className='submitBtn'>
-                        <Button id="landingButton" size="large" onClick={this.onSubmit}>
-                            Request Drawing Materials
-                    </Button>
-
-                    </div>
-
-                </MuiThemeProvider>
-            </form>
-                       <Footer/>
+      });
+    }
+  };
+  render() {
+    console.log('redux state is', this.props.store);
+    return (
+      <div id='app-container'>
+        <form className='centered'>
+          <MuiThemeProvider theme={theme}>
+            <HomeIcon
+              fontSize='large'
+              style={{ color: '#577590' }}
+              onClick={this.goHome}
+            />
+            <h2 className='title'>Get Drawing Materials!</h2>
+            <div className='centered'>
+              <h5 className='smallerTitle'>Location</h5>
+              <InputLabel id='location'>Where is your event?</InputLabel>
+              <Select
+                required
+                id='location'
+                defaultValue={''}
+                style={{ minWidth: 200 }}
+                onChange={(event) => this.onChange(event, 'location')}
+              >
+                <MenuItem value='' disabled>
+                  Select Event{' '}
+                </MenuItem>
+                {this.props.store.eventReducer.map((event) => {
+                  return (
+                    <MenuItem key={event.id} value={event.id}>
+                      {event.location}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
             </div>
 
-        );
-    }
+            <div className='centered'>
+              <h5 className='centered smallerTitle'>Table Number</h5>
+              <Input
+                style={{ minWidth: 200 }}
+                type='number'
+                placeholder='Table Number'
+                onChange={(event) => this.onChange(event, 'tableNumber')}
+                required
+              ></Input>
+            </div>
+
+            <div className='centered'>
+              <h5 className='smallerTitle'>Number of Participants</h5>
+              <Input
+                style={{ minWidth: 200 }}
+                type='number'
+                placeholder='Select a Number'
+                onChange={(event) => this.onChange(event, 'artistNumber')}
+                required
+              ></Input>
+            </div>
+            <div className='submitBtn'>
+              <Button id='landingButton' size='large' onClick={this.onSubmit}>
+                Request Drawing Materials
+              </Button>
+            </div>
+          </MuiThemeProvider>
+        </form>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default connect(mapStoreToProps)(UserMaterialRequest);
